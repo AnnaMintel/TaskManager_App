8680406bdad24f03d20be876e83605eb37d817fc
AnnaMintel <annikamintel99@yandex.ru>
2022-03-16 23:15:04 +0300

    feat: made state modification

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,26 @@ export type TaskType = {
     isDone: boolean
 }
 
-
 function App() {
-    const tasksOne: Array<TaskType> = [
-        {title: "learn JS", id: 1, isDone: false}, 
+    // BLL: business logic lair
+    let tasks: Array<TaskType> = [
+        {title: "learn JS", id: 1, isDone: false},  
         {title: "make makeUp", id: 2, isDone: true}, 
         {title: "go for a walk", id: 3, isDone: false}
     ];
-    const tasksTwo: Array<TaskType> = [
-        {title: "write essay", id: 1, isDone: false}, 
-        {title: "learn 15 words", id: 2, isDone: true}, 
-        {title: "make training IELTS test", id: 3, isDone: true}
-    ];
+
+    function removeTask(taskID: number) {
+        const filteredTasks = tasks.filter(t => t.id !== taskID) // true
+        tasks = filteredTasks
+        console.log(tasks)
+    }
     
+    //UI:
     return (
         <div className="App">
-           <TaskManager title={"Today's tasks"} tasks={tasksOne}/>
-           <TaskManager title={"English learning tasks"} tasks={tasksTwo}/>
+           <TaskManager title={"Today's tasks"} tasks={tasks}/>
+
+           {/* <TaskManager title={"English learning tasks"} tasks={tasksTwo}/> */}
         </div>
     );
 }
--- src/TaskManager.tsx
+++ src/TaskManager.tsx
@@ -4,10 +4,18 @@ import { TaskType } from './App';
 type TaskManagerType = {
     title: string
     tasks: Array<TaskType>
-
 }
 
 export const TaskManager = (props: TaskManagerType) => {
+    const tasks = props.tasks.map(task => {
+        return (
+            <li>
+                <input type="checkbox" checked={task.isDone} />
+                <span>{task.title}</span>
+            </li>
+        )
+    })
+
     return (
         <div>
             <h3>{props.title}</h3>
@@ -16,18 +24,19 @@ export const TaskManager = (props: TaskManagerType) => {
                 <button>+</button>
             </div>
             <ul>
-                <li>
-                    <input type="checkbox" checked={props.tasks[0].isDone} /> 
-                    <span>{props.tasks[0].title}</span>
-                </li>
-                <li>
-                    <input type="checkbox" checked={props.tasks[1].isDone} /> 
-                    <span>{props.tasks[1].title}</span>
-                </li>
-                <li>
-                    <input type="checkbox" checked={props.tasks[2].isDone} />
-                     <span>{props.tasks[2].title}</span>
-                </li>
+                {tasks}
+                {/* <li>
+                <input type="checkbox" checked={props.tasks[0].isDone} />
+                <span>{props.tasks[0].title}</span>
+            </li>
+            <li>
+                <input type="checkbox" checked={props.tasks[1].isDone} />
+                <span>{props.tasks[1].title}</span>
+            </li>
+            <li>
+                <input type="checkbox" checked={props.tasks[2].isDone} />
+                <span>{props.tasks[2].title}</span>
+            </li> */}
             </ul>
             <div>
                 <button>All</button>
