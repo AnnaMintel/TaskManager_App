937b86914fd2db1fe488f381a9d7c644e12c18ae
AnnaMintel <annikamintel99@yandex.ru>
2022-07-09 23:43:42 +0300

    feat: added callback and worked with useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
 import React, { useState } from 'react';
+import { v1 } from 'uuid';
 import './App.css';
 import { TaskManager } from './TaskManager';
 
 export type TaskType = {
     title: string
-    id: number
+    id: string
     isDone: boolean
 }
 
 export type FilterValuesType = "all" | "active" | "completed"
 
 function App() {
-
+    console.log(v1());
+    const [filter, setFilter] = useState<FilterValuesType>("all")
     const [tasks, setTasks] = useState<Array<TaskType>>(
         [
-        { id: 1, title: "learn JS", isDone: false },
-        { id: 2, title: "make makeUp", isDone: true },
-        { id: 3, title: "go for a walk", isDone: false }
+        { id: v1(), title: "learn JS", isDone: false },
+        { id: v1(), title: "make makeUp", isDone: true },
+        { id: v1(), title: "go for a walk", isDone: false }
     ])
 
-    const [filter, setFilter] = useState<FilterValuesType>("all")
-
-    function removeTask(taskID: number) {
+    function removeTask(taskID: string) {
         const filteredTasks = tasks.filter(task => task.id !== taskID) // true
         setTasks (filteredTasks)
         console.log(tasks)
@@ -29,6 +29,17 @@ function App() {
 
     function changeFilter(newFilterValue: FilterValuesType){
         setFilter(newFilterValue) 
+    }
+
+    function addTask(title: string){
+        const newTask: TaskType = {
+            id: v1(),
+            title,
+            isDone: false
+        }
+        const newTasks = [newTask, ...tasks];
+        setTasks(newTasks);
+        // setTasks([{id: v1(), title, isDone: false}, ...tasks]); // краткий вариант строк 35-41
     }
 
     let tasksForTaskManager = tasks
@@ -45,6 +56,7 @@ function App() {
             <TaskManager 
                 title={"Today's tasks"}
                 tasks={tasksForTaskManager}
+                addTask={addTask}
                 removeTask={removeTask}
                 changeFilter={changeFilter}
             />
--- src/TaskManager.tsx
+++ src/TaskManager.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FilterValuesType, TaskType } from './App';
 
 type TaskManagerType = {
     title: string
     tasks: Array<TaskType>
-    removeTask: (taskID: number) => void
+    removeTask: (taskID: string) => void
+    addTask: (title: string) => void
     changeFilter: (newFilterValue: FilterValuesType) => void
 }
 
 export const TaskManager = (props: TaskManagerType) => {
+    const [title, setTitle] = useState<string>(" ");
+
+    const onClickAddTask = () => {props.addTask(title), setTitle(" ")}
 
     const tasks = props.tasks.map(task => {
         return (
@@ -24,13 +28,13 @@ export const TaskManager = (props: TaskManagerType) => {
         <div>
             <h3>{props.title}</h3>
             <div>
-                <input />
-                <button>+</button>
-            </div>
-            <ul>
+                <input
+                    value={title}
+                    onChange={(e) => setTitle(e.currentTarget.value)} />
+                <button onClick={onClickAddTask}>+</button>
+        </div><ul>
                 {tasks}
-            </ul>
-            <div>
+            </ul><div>
                 <button onClick={() => props.changeFilter("all")}>All</button>
                 <button onClick={() => props.changeFilter("active")}>Active</button>
                 <button onClick={() => props.changeFilter("completed")}>Completed</button>
